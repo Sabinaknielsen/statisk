50acaf1e90e85de79f67d280049bc94f9e211067
Sabina <sani0001@stud.kea.dk>
2024-09-02 17:06:51 +0200

    hh

--- produkt.js
+++ produkt.js
@@ -1,21 +1,10 @@
-window.addEventListener("DOMContentLoaded", init);
+fetch("https://kea-alt-del.dk/t7/api/products/1163")
+.then(Response=>Response.json())
+.then(data=>showProduct(data));
 
-const id = 1532;
-const uri = `https://kea-alt-del.dk/t7/api/products/${id}`;
-const imgURI = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
 
-function init() {
-  fetch(uri)
-    .then((reponse) => reponse.json())
-    .then(handleData);
-}
+function showProduct(product){
+console.log(product);
 
-function handleData(json) {
-  console.log("json", json);
 
-  document.querySelector("h2").textContent = json.productdisplayname;
-  document.querySelector("p").textContent = json.brandbio;
-
-  document.querySelector("img").src = imgURI;
-  document.querySelector("img").alt = `Image of ${json.productdisplayname}`;
 }
