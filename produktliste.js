7ebec453277b7cb0e3793af5290d46f075d09bbf
Sabina <sani0001@stud.kea.dk>
2024-09-03 12:37:52 +0200

    jj

--- produktliste.js
+++ produktliste.js
@@ -1,10 +1,22 @@
+window.addEventListener("DOMContentLoaded", henData);
+
+
+
+const url = `https://kea-alt-del.dk/t7/api/products?limit=15`;
+const skabelon = document.querySelector("template")
+const container = document.querySelector("main")
+
 
 
     fetch("https://kea-alt-del.dk/t7/api/products")
     .then(res=>res.json())
 .then(showProducts)
 
-
+function henData(){
+fetch(url)
+.then((res) => res.json())
+.then((products)) => visProdukter(products));
+}
 function showProducts (products){
 //Looper og kalder showProduct
 products.forEach(showProducts)
@@ -16,9 +28,9 @@ function showProducts (product){
     console.log(product);
     //fang template
 const template = document.querySelector("#smallProductTemplate").content;
+
     //lav en kopi
 const copy = template.cloneNode(true)
-    //ændre indhold
     copy.querySelector("h2").textcontet=product.productdisplayname;
 document.querySelector("main ").appendChild(copy);
 if(product.soldout) {
